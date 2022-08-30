003e64900d48bf260349a8fdb46b211f923e1e54
Ginger Liang <ginger@citiesocial.com>
2022-08-31 03:29:12 +0800

    remove background

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.browserAction.onClicked.addListener(function (activeTab) {
-  var newURL =
-    'https://drip.crowdfunding.coffee/landingpage.php?utm_source=plugin'
-  chrome.tabs.create({ url: newURL })
-})
